ce67db992e5b060f831b33a087e721bea3266d5d
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2022-07-25 05:13:34 +0900

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang= "ko">
+<head>
+<meta charset="UTF-8">
+<title>JavaScript Syntax</title>
+</head>
+
+<body>
+<h1>자바스크립트 주석</h1>
+<p id="result"></p>
+<script>
+var x = 10;
+//var y = 20; 한줄의 주석 문 이 부분은 실행x
+/*
+x=x+y;
+여러줄의 주석문:
+역시 실행x
+*/
+document.getElementByld("result").innerHTML=x;
+</script>
+</body>
+</html>
